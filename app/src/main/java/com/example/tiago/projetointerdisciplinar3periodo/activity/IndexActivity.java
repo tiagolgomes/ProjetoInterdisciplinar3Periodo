4fd5fa22da23130b74a35b0aa63c3327d31b17d3
Tiago Gomes <tiagolacerdagomes@gmail.com>
2018-11-26 15:44:44 -0200

    Ajuste de Telas para Print

--- app/src/main/java/com/example/tiago/projetointerdisciplinar3periodo/activity/IndexActivity.java
+++ app/src/main/java/com/example/tiago/projetointerdisciplinar3periodo/activity/IndexActivity.java
@@ -1,7 +1,9 @@
 package com.example.tiago.projetointerdisciplinar3periodo.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.tiago.projetointerdisciplinar3periodo.R;
 
@@ -13,5 +15,11 @@ public class IndexActivity extends Activity {
         setContentView(R.layout.activity_index);
 
 
+    }
+
+    public void callNovoEstab(View View){
+        Intent novoEstab = new Intent(IndexActivity.this,NovoEstabelecimentoActivity.class);
+        startActivity(novoEstab);
+        finish();
     }
 }
